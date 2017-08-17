bfa3b20af6598f275b9540ced40855cb21e0396d
Jessa Marie Barre <jmlbarre@gmail.com>
2017-08-16 21:16:10 -0400

    Added migrations

--- db/migrate/20170817005058_create_songs.rb
+++ db/migrate/20170817005058_create_songs.rb
@@ -1,7 +1,10 @@
 class CreateSongs < ActiveRecord::Migration[5.1]
   def change
     create_table :songs do |t|
-
+      t.string :title
+      t.string :album
+      t.string :artist
+      t.string :song_link
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Song.destroy_all
+
+Song.new(
+  title: "Can't Help Falling in Love",
+  album: "Better",
+  artist: "Hayley Reinhart",
+  song_link: "https://www.youtube.com/watch?v=jnqfr-wxfsw"
+)
+
+Song.new(
+  title: "Watching You",
+  album: "If You're Going Through Hell",
+  artist: "Rodney Atkins",
+  song_link: "https://www.youtube.com/watch?v=2uzK3VwzraM"
+)
+
+Song.new(
+  title: "Can't Help Falling in Love",
+  album: "Better",
+  artist: "Hayley Reinhart",
+  song_link: "https://www.youtube.com/watch?v=jnqfr-wxfsw"
+)
+
+Song.new(
+  title: "Aisuru Anata e",
+  album: "Tegami",
+  artist: "Maiko Fujita",
+  song_link: "https://www.youtube.com/watch?v=iuOI6zP0hew"
+)
+
+Song.new(
+  title: "Only Jesus",
+  album: "After All These Years",
+  artist: "Brian Johnson",
+  song_link: "https://www.youtube.com/watch?v=zxgBwDXTmEA"
+)
